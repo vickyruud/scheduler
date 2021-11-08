6a83db0b5609dbef985e639f8af50e35da8624d9
vickyruud <aprajit.k@gmail.com>
2021-11-08 14:59:12 -0500

    add InterviwerList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,7 +3,7 @@ import DayListItem from "./DayListItem";
 // 
 
 export default function DayList(props) {
-  
+
   const days = props.days.map(day => {
     return (
       <DayListItem
@@ -11,7 +11,7 @@ export default function DayList(props) {
         name={day.name}
         spots={day.spots}
         selected={day.name === props.day}
-        setDay={props.setDay}
+        setDay={set => props.setDay(day.name)}
       />
     );
   });
