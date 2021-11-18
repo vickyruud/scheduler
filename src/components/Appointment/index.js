6cf96ff5057b115e4ec66fc6a632482f7868a6de
vickyruud <aprajit.k@gmail.com>
2021-11-18 11:11:41 -0500

    add show empty create modes and views

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,18 +5,29 @@ import "./styles.scss";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 
 
 export default function Appointment(props) {
 
-    // const {student, interviewer} = props.interview
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const interviewers = [];
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );  
 
   return (
     <>
     <article className="appointment">
     <Header time = {props.time} />
-    {props.interview ? <Show student = {props.interview["student"]} interviewer = {props.interview.interviewer}  /> : <Empty /> }
+    {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+    {mode === SHOW && props.interview && <Show student = {props.interview["student"]} interviewer = {props.interview.interviewer}  />}
+    {mode === CREATE && <Form interviewers = {interviewers}/>}
     </article> 
     
     </>
